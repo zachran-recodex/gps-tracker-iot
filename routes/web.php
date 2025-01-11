8a1cc59333fa4db1bc89270536ea1783bff523c0
Zachran Razendra <zachranraze@recodex.id>
2025-01-12 05:26:15 +0700

    Remove unused GpsLocationController import from web routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\GpsLocationController;
 
 Route::get('/', function () {
     return redirect()->route('login');
