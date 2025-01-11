a49e46ef9503fe2b4f7e9fa3f12aac0f3059ca04
Zachran Razendra <zachranraze@recodex.id>
2025-01-12 05:24:09 +0700

    Remove account deletion functionality from profile management

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ Route::get('/', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/locations', [DashboardController::class, 'getLocations'])->name('locations.get');
